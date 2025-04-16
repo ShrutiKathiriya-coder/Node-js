1d40345deb0ed0d2e0b5761ae6b8e960cb155cdb
shruti kathiriya <kathiriyashruti3@gmail.com>
2025-04-16 11:54:18 +0530

    Update Artwork.js

--- project-4  art-gallery/models/Artwork.js
+++ project-4  art-gallery/models/Artwork.js
@@ -1,11 +1,25 @@
 const mongoose = require('mongoose');
 
 const artworkSchema = new mongoose.Schema({
-  art_title: String,
-  artist_name: String,
-  creation_year: Number,
-  art_type: String,
-  art_image: String,
-});
+  art_title: {
+    name:string,
+    require:true,
+  },
+   artist_name: {
+    name:string,
+    require:true,
+  },
+   creation_year: {
+    name:number,
+    require:true,
+  },
+   art_type: {
+    name:string,
+    require:true,
+  },
+   art_image: {
+    name:string,
+    require:true,
+  }
 
 module.exports = mongoose.model('Artwork', artworkSchema);
